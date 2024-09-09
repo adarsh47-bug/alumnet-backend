b9459886ffaf597ecadf36c68141b5384661c786
Adarsh Kadam <120251944+adarsh47-bug@users.noreply.github.com>
2024-09-10 01:57:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const jobRoutes = require('./routes/jobRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+
+const corsOptions = {
+  origin: 'https://your-frontend-static-web-app.azurestaticapps.net', // Replace with your frontend domain
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+  res.send('API is running... zz');
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/jobs', jobRoutes);
